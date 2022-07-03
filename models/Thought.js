82b94e56b16a9cbd7e9dcbac7ea8ac83952e4101
Alejandro Villanueva <aavillanueva6@gmail.com>
2022-07-03 14:35:39 -0700

    adds initial thought model

---
+++ models/Thought.js
@@ -0,0 +1,34 @@
+const { Schema, model } = require('mongoose');
+
+// Schema to create User model
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionsSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+  }
+);
+
+thoughtSchema.virtual('reactionCount').get(function () {
+  return this.reactions.length;
+});
+
+const Thought = model('thought', thoughtSchema);
+module.exports = Thought;
