638c7a84fa22fb453f7c955b479b28259a65812b
Alejandro Villanueva <aavillanueva6@gmail.com>
2022-07-03 17:52:35 -0700

    fixes error in not exporting the router function

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -24,3 +24,5 @@ router.route('/:thoughtId/reactions').post(addReaction);
 
 // api/thoughts/:thoughtId/reactions/:reactionId
 router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
+
+module.exports = router;
