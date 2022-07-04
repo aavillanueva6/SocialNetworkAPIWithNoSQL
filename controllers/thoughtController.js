68258436b98c23844a3b91c44e0f870867e2bef9
Alejandro Villanueva <aavillanueva6@gmail.com>
2022-07-03 19:45:14 -0700

    updates placeholder routes handlers, adds a few user route handlers

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -3,35 +3,29 @@ const { User, Thought } = require('../models');
 module.exports = {
   // Get all thoughts
   getThoughts(req, res) {
-    Thought.find()
-      .then(async (thoughts) => {
-        const thoughtObj = {
-          thoughts,
-          headCount: await headCount(),
-        };
-        return res.json(thoughtObj);
-      })
+    Thought.find({})
+      .then((users) => res.status(200).json(users))
       .catch((err) => {
-        console.log(err);
-        return res.status(500).json(err);
+        console.error(err);
+        res.status(500).json(err);
       });
   },
   getSingleThought(req, res) {
-    return res.status(200);
+    return res.status(200).send('');
   },
   createThought(req, res) {
-    return res.status(200);
+    return res.status(200).send('');
   },
   updateThought(req, res) {
-    return res.status(200);
+    return res.status(200).send('');
   },
   deleteThought(req, res) {
-    return res.status(200);
+    return res.status(200).send('');
   },
   addReaction(req, res) {
-    return res.status(200);
+    return res.status(200).send('');
   },
   deleteReaction(req, res) {
-    return res.status(200);
+    return res.status(200).send('');
   },
 };
--- controllers/userController.js
+++ controllers/userController.js
@@ -3,35 +3,44 @@ const { User, Thought } = require('../models');
 module.exports = {
   // Get all users
   getUsers(req, res) {
-    User.find()
-      .then(async (users) => {
-        const userObj = {
-          users,
-          headCount: await headCount(),
-        };
-        return res.json(userObj);
-      })
+    User.find({})
+      .then((users) => res.status(200).json(users))
       .catch((err) => {
-        console.log(err);
-        return res.status(500).json(err);
+        console.error(err);
+        res.status(500).json(err);
       });
   },
+  // Get a single user
   getSingleUser(req, res) {
-    return res.status(200);
+    User.findOne({ _id: req.params.userId })
+      .select('-__v')
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'No user with that ID' })
+          : res.status(200).json(user)
+      )
+      .catch((err) => res.status(500).json(err));
   },
+  // Create a new user
   createUser(req, res) {
-    return res.status(200);
+    User.create(req.body)
+      .then((dbUserData) => res.status(200).json(dbUserData))
+      .catch((err) => res.status(500).json(err));
   },
+  // Update a single user
   updateUser(req, res) {
-    return res.status(200);
+    return res.status(200).send('');
   },
+  // Delete a specified user
   deleteUser(req, res) {
-    return res.status(200);
+    return res.status(200).send('');
   },
+  // Add friend to user
   addFriend(req, res) {
-    return res.status(200);
+    return res.status(200).send('');
   },
+  // Delete friend from user
   deleteFriend(req, res) {
-    return res.status(200);
+    return res.status(200).send('');
   },
 };
